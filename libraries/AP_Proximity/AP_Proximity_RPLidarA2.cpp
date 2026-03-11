89d962439c6cec355177ecd7a2c6f3ba5690c5ce
BahaAyt <114946669+BahaAyt@users.noreply.github.com>
2026-03-11 15:52:00 +0300

    Log RPLidar model and firmware information
    Added a debug message to get raw model info from ardupilot to gcs.

--- libraries/AP_Proximity/AP_Proximity_RPLidarA2.cpp
+++ libraries/AP_Proximity/AP_Proximity_RPLidarA2.cpp
@@ -361,6 +361,10 @@ void AP_Proximity_RPLidarA2::parse_response_device_info()
     default:
         Debug(1, "Unknown device (%u)", _payload.device_info.model);
     }
+    GCS_SEND_TEXT(MAV_SEVERITY_INFO,
+              "RPLidar raw model=%u (0x%02X)",
+              _payload.device_info.model,
+              _payload.device_info.model);
     GCS_SEND_TEXT(MAV_SEVERITY_INFO, "RPLidar %s hw=%u fw=%u.%u", device_type, _payload.device_info.hardware, _payload.device_info.firmware_minor, _payload.device_info.firmware_major);
     send_scan_mode_request();
     _state = State::AWAITING_RESPONSE;
