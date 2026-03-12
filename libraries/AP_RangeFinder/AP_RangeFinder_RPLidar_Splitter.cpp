569cf2123e6c4352b832621c3b657aa1239a0e16
BahaAyt <114946669+BahaAyt@users.noreply.github.com>
2026-03-12 16:28:35 +0300

    Update status handling for proximity sensor

--- libraries/AP_RangeFinder/AP_RangeFinder_RPLidar_Splitter.cpp
+++ libraries/AP_RangeFinder/AP_RangeFinder_RPLidar_Splitter.cpp
@@ -87,13 +87,13 @@ void AP_RangeFinder_RPLidar_Splitter::update(void)
 
     // check for timeout and set health status
     if (AP_HAL::millis() - _last_distance_received_ms > COMM_ACTIVITY_TIMEOUT_MS) {
-        set_status(AP_Rangefinder::Status::NoData);
+        set_status(AP_Proximity::Status::NoData);
         Debug(1, "LIDAR NO DATA");
         if (AP_HAL::millis() - _last_reset_ms > 10000) {
             reset_rplidar();
         }
     } else {
-        set_status(AP_Rangefinder::Status::Good);
+        set_status(AP_Proximity::Status::Good);
     }
 }
 
